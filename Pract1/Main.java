06abccd3e87731ae4f9cf3babed0e7cf1bd06d65
User <baydak.danila00@mail.ru>
2023-04-05 08:53:02 +0300

    Initial commit

---
+++ Pract1/Main.java
@@ -0,0 +1,14 @@
+package Pract1;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "843jrej43f";
+        String s2 = "1438hrje34";
+
+        Comparator<String> comparator = Comparator.comparingInt(o ->
+                o.chars().filter(Character::isDigit).map(Character::getNumericValue).sum());
+
+        System.out.println(comparator.compare(s1, s2));
+    }
+}
